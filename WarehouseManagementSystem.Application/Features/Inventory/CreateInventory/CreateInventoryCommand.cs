878ce8717597927197b2abd407aa7171528cecdf
akai0411 <jandresmargolles@gmail.com>
2026-06-29 12:05:35 +0900

    feat: feat: add CreateInventoryCommand with validator

---
+++ WarehouseManagementSystem.Application/Features/Inventory/CreateInventory/CreateInventoryCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace WarehouseManagementSystem.Application.Features.Inventory.CreateInventory
+{
+    public class CreateInventoryCommand
+    {
+        public Guid ProductId { get; set; }
+        public Guid LocationId { get; set; }
+        public int Quantity { get; set; }
+        public int MinimumStockLevel { get; set; }
+    }
+}
