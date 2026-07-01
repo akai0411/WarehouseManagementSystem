7a37f825e36d1e9fba9995f1674b21f89c0f8714
akai0411 <jandresmargolles@gmail.com>
2026-07-01 14:14:58 +0900

    feat(stockMovement): added validator and handler

---
+++ WarehouseManagementSystem.Application/Features/StockMovements/CreateStockMovement/CreateStockMovementValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using WarehouseManagementSystem.Domain.Entities;
+
+namespace WarehouseManagementSystem.Application.Features.StockMovements.CreateStockMovement
+{
+    public class CreateStockMovementCommandValidator
+        : AbstractValidator<CreateStockMovementCommand>
+    {
+        private static readonly string[] ValidTypes =
+            Enum.GetNames(typeof(MovementType));
+
+        public CreateStockMovementCommandValidator()
+        {
+            RuleFor(x => x.InventoryId)
+                .NotEmpty().WithMessage("InventoryId is required.");
+
+            RuleFor(x => x.Type)
+                .NotEmpty().WithMessage("Movement type is required.")
+                .Must(t => ValidTypes.Contains(t))
+                .WithMessage($"Type must be one of: {string.Join(", ", ValidTypes)}.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than 0.");
+
+            RuleFor(x => x.Reference)
+                .MaximumLength(100)
+                .WithMessage("Reference cannot exceed 100 characters.");
+
+            RuleFor(x => x.Notes)
+                .MaximumLength(500)
+                .WithMessage("Notes cannot exceed 500 characters.");
+        }
+    }
+}
